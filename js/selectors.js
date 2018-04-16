60da261ce2781686573969ff920c0dc8d614ab5a
Andrew Nikonchuk <anikonch@gmail.com>
2018-04-16 17:42:48 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -7,3 +7,15 @@ function paragraphSelector(){
 function lastImageSelector(){
   return $('img:last');
 }
+
+function ninjaBabySelector(){
+  return $('#baby-ninja');
+}
+
+function divSelector() {
+  return $('.pics');
+}
+
+function firstListItem() {
+  return $('#pic-list li:first-child')
+}
